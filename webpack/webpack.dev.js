fcf49834802e3ecc05640ca221fb2c02182be801
ivanhristov92 <ivan_hristov@mail.bg>
2018-11-01 19:46:35 +0200

    refactoring

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -1,26 +1,29 @@
-const webpack = require("webpack");
+const webpack = require('webpack');
 
-const commonPaths = require("./paths");
+const commonPaths = require('./paths');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   output: {
-    filename: "[name].js",
+    filename: '[name].js',
     path: commonPaths.outputPath,
-    chunkFilename: "[name].js"
+    chunkFilename: '[name].js',
   },
   module: {
     rules: [
       {
         test: /\.(css|scss)$/,
-        use: ["style-loader", "css-loader"]
-      }
-    ]
+        use: [
+          'style-loader',
+          'css-loader',
+        ],
+      },
+    ],
   },
   devServer: {
     contentBase: commonPaths.outputPath,
     compress: true,
-    hot: true
+    hot: true,
   },
-  plugins: [new webpack.HotModuleReplacementPlugin()]
+  plugins: [new webpack.HotModuleReplacementPlugin()],
 };
