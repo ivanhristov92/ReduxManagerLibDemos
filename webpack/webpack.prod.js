0338d9bd3b0c3130b012cfef31a3f7db729741d4
ivanhristov92 <ivan_hristov@mail.bg>
2018-11-05 14:35:43 +0200

    updating

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,42 +1,30 @@
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
-const commonPaths = require('./paths');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const commonPaths = require("./paths");
 
 module.exports = {
-  mode: 'production',
+  mode: "production",
   output: {
     filename: `${commonPaths.jsFolder}/[name].[hash].js`,
     path: commonPaths.outputPath,
-    chunkFilename: '[name].[chunkhash].js',
+    chunkFilename: "[name].[chunkhash].js"
   },
   module: {
     rules: [
       {
         test: /\.(css|scss)$/,
-        use: [
-          MiniCssExtractPlugin.loader,
-          {
-            loader: 'css-loader',
-            options: {
-              sourceMap: true,
-              modules: true,
-              camelCase: true,
-              localIdentName: '[local]___[hash:base64:5]',
-            },
-          },
-          'sass-loader',
-        ],
-      },
-    ],
+        use: ["style-loader", "css-loader"]
+      }
+    ]
   },
   plugins: [
-    new CleanWebpackPlugin([commonPaths.outputPath.split('/').pop()], {
-      root: commonPaths.root,
+    new CleanWebpackPlugin([commonPaths.outputPath.split("/").pop()], {
+      root: commonPaths.root
     }),
     new MiniCssExtractPlugin({
       filename: `${commonPaths.cssFolder}/[name].css`,
-      chunkFilename: '[id].css',
-    }),
+      chunkFilename: "[id].css"
+    })
   ],
-  devtool: 'source-map',
+  devtool: "source-map"
 };
